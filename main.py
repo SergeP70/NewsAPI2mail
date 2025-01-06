50460340f0d01d967ee0ec1e5748901735b1751a
sergeP70 <serge@pille.be>
2025-01-06 23:18:56 +0100

    Access news API and get data

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import requests
 
-url = 'https://finance.yahoo.com'
+api_key = '044315ef4a2e4f5784f982be3b6a15c4'
+url = ('https://newsapi.org/v2/everything?q=apple'
+       '&from=2025-01-05&to=2025-01-05&sortBy=popularity&apiKey=044315ef4a2e4f5784f982be3b6a15c4')
+
 request = requests.get(url)
-content = request.text
-print(content)
+content = request.json()
+
+for article in content['articles']:
+    print(article['title'])
+
+
+
+
+
+
 
 
