9798a37905b66877528aa5d60c6e737ab57522ca
sergeP70 <serge@pille.be>
2025-01-08 08:27:52 +0100

    Sending mail with news info

---
+++ mailer.py
@@ -0,0 +1,31 @@
+# install SSL certificate when SSL-error
+# install pip python-dotenv
+# /Applications/Python/3.12/Install/Certificates.command with admin privileges
+import smtplib
+import ssl
+from email.message import EmailMessage
+# import environment variables:
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+my_context = ssl.create_default_context()
+
+
+def send(receiver, subject, message):
+    mail = EmailMessage()
+    mail['From'] = username
+    mail['To'] = receiver
+    mail['Subject'] = subject
+    message = message + "\n This is a message from: " + receiver
+    mail.set_content(message)
+
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.send_message(mail)
+        server.quit()
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import mailer
 
 api_key = '044315ef4a2e4f5784f982be3b6a15c4'
 url = ('https://newsapi.org/v2/everything?q=apple'
@@ -6,14 +7,15 @@ url = ('https://newsapi.org/v2/everything?q=apple'
 
 request = requests.get(url)
 content = request.json()
+receiver = "sergepille70@gmail.com"
+subject = "News from Apple"
+message = "This are the bullet points from Apple: \n\n"
 
 for article in content['articles']:
-    print(article['title'])
+    # print(article['description'])
+    message = message + article['title'] + '\n' + str(article['description']) + '\n\n'
 
 
-
-
-
-
-
+print(message)
+mailer.send(receiver, subject, message)
 
