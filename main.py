294a440c7ebeeb1259614714f429a01555a621c4
sergeP70 <serge@pille.be>
2025-01-08 23:52:39 +0100

    add links, select language, limit news, control topic

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import requests
 import mailer
+from datetime import date
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+topic = "microsoft"
+today = date.today()
+print(today)
+# load only the first 15 and english topics:
 api_key = '044315ef4a2e4f5784f982be3b6a15c4'
-url = ('https://newsapi.org/v2/everything?q=apple'
-       '&from=2025-01-05&to=2025-01-05&sortBy=popularity&apiKey=044315ef4a2e4f5784f982be3b6a15c4')
+url = (f'https://newsapi.org/v2/everything?q={topic}'
+       f'&from=2025-01-05&to={today}'
+       '&sortBy=publishedAt'
+       '&language=en'
+       '&pageSize=15'
+       '&apiKey=044315ef4a2e4f5784f982be3b6a15c4')
 
 request = requests.get(url)
 content = request.json()
-receiver = "sergepille70@gmail.com"
-subject = "News from Apple"
-message = "This are the bullet points from Apple: \n\n"
+
+receiver = os.getenv('USERNAME')
+subject = f"News from {topic.title()}"
+body = f"This are the bullet points from {topic}: \n\n"
 
 for article in content['articles']:
-    # print(article['description'])
-    message = message + article['title'] + '\n' + str(article['description']) + '\n\n'
+    body = body + article['title'] + '\n' + str(article['description']) + '\n'
+    body = body + article['url'] +'\n\n'
 
+print(body)
+mailer.send(receiver, subject, body)
 
-print(message)
-mailer.send(receiver, subject, message)
+"""
+1. Add link
+2. Show less news
+3. Show only news in English
+4. Add Subject
+5. Dynamic content, add new link
 
+"""
